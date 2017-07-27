d00dca7bac2c802ff45f27229dd11d9ab031138e
Alexandre <alexandrehsato@gmail.com>
2017-07-27 00:34:33 -0300

    alterando para passar o parametro do produto para a tela de mostra.

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -8,7 +8,7 @@
 	<div class="container">
 		<h1>Lista de produtos com Laravel</h1>
 		<table class="table table-striped table-bordered">
-		<?php 
+		<?php 		
 			foreach($produtos as $p):
 		 ?>
 			 <tr>
@@ -16,7 +16,11 @@
 			 	<td><?=$p->valor?></td>
 			 	<td><?=$p->descricao?></td>
 			 	<td><?=$p->quantidade?></td>
-			 	<td><a href="/produtos/mostra"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></td>
+			 	<td>
+			 		<a href="/produtos/mostra?id=<?=$p->ID?>">
+			 			<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+			 		</a>
+			 	</td>
 			 	<td><a href="/produtos/mostra"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a></td>
 			 	
 			 </tr>
